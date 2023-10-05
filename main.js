a806bd5dd64f4f9c8706dce65ddd071982347970
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-10-05 11:28:44 +0200

    FIX: sitemap

--- main.js
+++ main.js
@@ -236,13 +236,13 @@ async function init() {
         console.log(`\x1b[34mÉtape 0:\x1b[0m Dossier "${outputDirectory}" créé.`);
 
         const relativePath = path.join(outputDirectory, 'index.html');
+        await downloadResource(`${siteUrl}/sitemap.xml`, path.join(outputDirectory, '/sitemap.xml'), 1, 1, 1);
+        await fs.mkdir(path.join(outputDirectory, '/wp-content/uploads/wdseo_sitemaps'), { recursive: true });
+        await downloadResource(`${siteUrl}/wp-content/uploads/wdseo_sitemaps/sitemap.xml`, path.join(outputDirectory, '/wp-content/uploads/wdseo_sitemaps/sitemap.xml'), 1, 1, 1);
         await downloadResource(siteUrl, relativePath, 1, 1, 1);
-
+        
         let allLinks = [];
         let newLinks = await processFiles(outputDirectory, allLinks);
-
-        newLinks.push(`${siteUrl}/wp-content/uploads/wdseo_sitemaps/sitemap.xml`);
-        newLinks.push(`${siteUrl}/sitemap.xml`);
 
         while (newLinks.length > 0) {
             if (newLinks.length == 1) {
